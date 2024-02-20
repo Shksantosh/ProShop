c03d1e9ca79c4485544c7d660789af8876271533
shksantosh <santoshking82@gmail.com>
2024-02-20 13:03:35 +0530

    cartSlice clean up with utility functions

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,44 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { updateCart } from '../utils/cartUtils';
 
 const initialState = localStorage.getItem("cart") ? JSON.parse
     (localStorage.getItem("cart")) : { cartItems: [] }
-
-const addDecimals = (num) => {
-    return (Math.round(num * 100) / 100).toFixed(2)
-}
 
 const cartSlice = createSlice({
     name: "cart",
     initialState,
     reducers: {
         addToCart: (state, action) => {
+            // The item to add to the cart
             const item = action.payload
 
-            const existItem = state.cartItems.find((x) => x._id === item._id)
-            if (existItem) {
-                state.cartItems = state.cartItems.map((x) => x._id ===
-                    existItem._id ? item : x)
-            } else {
-                state.cartItems = [...state.cartItems, item]
-            }
-
-            // Calculate item price
-            state.itemsPrice = addDecimals(state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0))
-
-            // Calculate shipping price (If order is over ₹100 then free, else ₹10 shipping)
-            state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10)
-
-            // Calculate tax price (15% tax)
-            state.taxPrice = addDecimals(Number((0.15 * state.itemsPrice).toFixed(2)))
-
-            // Calculate total price
-            state.totalPrice = (
-                Number(state.itemsPrice) +
-                Number(state.shippingPrice) +
-                Number(state.taxPrice)
-            ).toFixed(2)
-
-            localStorage.setItem('cart', JSON.stringify(state))
+            // Update the cart state using the updateCart function
+            return updateCart(state, item);
         },
     },
 })
