0843345574ee9146b72e5887284d18627c01965e
shksantosh <santoshking82@gmail.com>
2024-02-20 23:30:16 +0530

    Logout User & Clear Cookie

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -41,11 +41,16 @@ const registerUser = asyncHandler(async (req, res) => {
     res.send('register user');
 });
 
-// @desc    Logout user
+// @desc    Logout user / clear cookie
 // @route   POST /api/users/logout
 // @access  Public
 const logoutUser = asyncHandler(async (req, res) => {
-    res.send('logout user');
+    res.cookie('jwt', '', {
+        httpOnly: true,
+        expires: new Date(0),
+    });
+    res.status(200).json({ message: 'Logged out successfully' });
+
 });
 
 // @desc    Get user profile
