2cde1646f8575bb3f800926cc849050b44e63b61
shksantosh <santoshking82@gmail.com>
2024-02-21 20:53:12 +0530

    Login Functionality

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -1,16 +1,45 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { Form, Button, Row, Col } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import Loader from '../components/Loader';
 import FormContainer from '../components/FormContainer';
+
+import { useLoginMutation } from '../slices/userApiSlice';
+import { setCredentials } from '../slices/authSlice';
+import { toast } from 'react-toastify';
 
 const LoginScreen = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    const submitHandler = (e) => {
-        e.preventDefault()
-        console.log('sumbit');
-    }
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const [login, { isLoading }] = useLoginMutation();
+
+    const { userInfo } = useSelector((state) => state.auth);
+
+    const { search } = useLocation();
+    const sp = new URLSearchParams(search);
+    const redirect = sp.get('redirect') || '/';
+
+    useEffect(() => {
+        if (userInfo) {
+            navigate(redirect);
+        }
+    }, [navigate, redirect, userInfo]);
+
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await login({ email, password }).unwrap();
+            dispatch(setCredentials({ ...res }));
+            navigate(redirect);
+        } catch (err) {
+            toast.error(err?.data?.message || err.error);
+        }
+    };
 
     return (
         <FormContainer>
@@ -37,16 +66,18 @@ const LoginScreen = () => {
                     ></Form.Control>
                 </Form.Group>
 
-                <Button type='submit' variant='primary' className='mt-2'>
+                <Button type='submit' variant='primary' className='mt-2' disabled={isLoading}>
                     Sign In
                 </Button>
+
+                {isLoading && <Loader />}
 
             </Form>
 
             <Row className='py-3'>
                 <Col>
                     New Customer?{' '}
-                    <Link to='/register'>
+                    <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>
                         Register
                     </Link>
                 </Col>
