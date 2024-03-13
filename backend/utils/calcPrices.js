7f7ed2fe6488648f806fb42ceab33d2f59967bbb
shksantosh <santoshking82@gmail.com>
2024-03-13 21:02:45 +0530

    fix: server calculates prices

--- backend/utils/calcPrices.js
+++ backend/utils/calcPrices.js
@@ -2,28 +2,20 @@ function addDecimals(num) {
     return (Math.round(num * 100) / 100).toFixed(2);
 }
 
-
 export function calcPrices(orderItems) {
-
-    // Calculate the items price in whole number (pennies) to avoid issues with
-    // floating point number calculations
-
-    const itemsPrice = orderItems.reduce(
-        (acc, item) => acc + (item.price * 100 * item.qty) / 100,
-        0
-    )
-
+    // Calculate the items price
+    const itemsPrice = addDecimals(
+        orderItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+    );
     // Calculate the shipping price
-    const shippingPrice = itemsPrice > 100 ? 0 : 10;
+    const shippingPrice = addDecimals(itemsPrice > 100 ? 0 : 10);
     // Calculate the tax price
-    const taxPrice = 0.15 * itemsPrice;
+    const taxPrice = addDecimals(Number((0.15 * itemsPrice).toFixed(2)));
     // Calculate the total price
-    const totalPrice = itemsPrice + shippingPrice + taxPrice;
-    // return prices as strings fixed to 2 decimal places
-    return {
-        itemsPrice: addDecimals(itemsPrice),
-        shippingPrice: addDecimals(shippingPrice),
-        taxPrice: addDecimals(taxPrice),
-        totalPrice: addDecimals(totalPrice),
-    };
+    const totalPrice = (
+        Number(itemsPrice) +
+        Number(shippingPrice) +
+        Number(taxPrice)
+    ).toFixed(2);
+    return { itemsPrice, shippingPrice, taxPrice, totalPrice };
 }
